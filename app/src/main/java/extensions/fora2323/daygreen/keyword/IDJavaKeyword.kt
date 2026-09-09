a95b0ef423841c16af01621e62420cccf7dfae9e
fora2323 <fora060823@gmail.com>
2026-09-10 03:45:25 +0800

    release fixed keyword more complete, xml analysis accurate 60% kotlin 50% js 15% css 15% html 15%

--- app/src/main/java/extensions/fora2323/daygreen/keyword/IDJavaKeyword.kt
+++ app/src/main/java/extensions/fora2323/daygreen/keyword/IDJavaKeyword.kt
@@ -1,51 +1,60 @@
 package extensions.fora2323.daygreen.keyword
 
+import kotlin.jvm.JvmField
+
 object IDJavaKeyword {
     @JvmField
     val KEYWORDS = arrayOf(
-        // Java keywords
+        // Java Keywords
         "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class",
         "const", "continue", "default", "do", "double", "else", "enum", "extends", "final",
         "finally", "float", "for", "goto", "if", "implements", "import", "instanceof",
         "int", "interface", "long", "native", "new", "package", "private", "protected",
         "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized",
         "this", "throw", "throws", "transient", "try", "void", "volatile", "while",
+        "var", "record", "sealed", "permits", "yield",
 
-        // Android classes
-        "Activity", "AppCompatActivity", "Fragment", "FragmentActivity", "Service",
-        "BroadcastReceiver", "ContentProvider", "Intent", "Bundle", "View", "TextView",
+        // Android Core & AndroidX Classes
+        "Activity", "AppCompatActivity", "ComponentActivity", "Fragment", "FragmentActivity",
+        "Service", "BroadcastReceiver", "ContentProvider", "Intent", "Bundle", "View", "TextView",
         "Button", "ImageView", "EditText", "RecyclerView", "ListView", "ScrollView",
         "RelativeLayout", "LinearLayout", "FrameLayout", "ConstraintLayout", "CardView",
-        "Toolbar", "AppBarLayout", "DrawerLayout", "NavigationView", "BottomNavigationView",
-        "Dialog", "AlertDialog", "Toast", "Snackbar", "ProgressBar", "SeekBar",
-        "Switch", "CheckBox", "RadioButton", "RadioGroup", "Spinner", "Adapter",
+        "Toolbar", "AppBarLayout", "DrawerLayout", "NavigationView",
+        "Dialog", "AlertDialog", "Toast", "ProgressBar", "SeekBar", "Adapter",
+        "Context", "SharedPreferences", "Lifecycle", "ViewModel", "LiveData",
 
-        // Common Android methods
+        // AndroidX Material Design 3 Components
+        "MaterialButton", "MaterialButtonToggleGroup", "ExtendedFloatingActionButton",
+        "FloatingActionButton", "MaterialCardView", "MaterialShapeDrawable",
+        "TopAppBar", "CenterAlignedTopAppBar", "MediumTopAppBar", "LargeTopAppBar",
+        "NavigationBar", "NavigationRail", "NavigationDrawer", "ModalNavigationDrawer",
+        "PermanentNavigationDrawer", "ModalBottomSheet", "BottomSheetDialog", "BottomSheetBehavior",
+        "Chip", "ChipGroup", "FilterChip", "InputChip", "SuggestionChip",
+        "SegmentedButton", "SegmentedButtonGroup",
+        "Slider", "RangeSlider", "Switch", "MaterialCheckBox", "MaterialRadioButton",
+        "BadgeDrawable", "BadgedBox", "LinearProgressIndicator", "CircularProgressIndicator",
+        "SearchBar", "DockedSearchBar", "Snackbar", "SnackbarHost",
+        "MaterialTimePicker", "MaterialDatePicker", "TextInputLayout", "TextInputEditText",
+        "MaterialDivider", "MaterialAutoCompleteTextView",
+
+        // Common Android Methods
         "findViewById", "setContentView", "onCreate", "onStart", "onResume", "onPause",
         "onStop", "onDestroy", "onClick", "setOnClickListener", "startActivity",
         "putExtra", "getIntent", "getExtras", "finish", "setText", "getText",
 
-        // R.java references
-        "R.id.", "R.layout.", "R.string.", "R.drawable.", "R.color.", "R.dimen.",
-        "R.style.", "R.attr.", "R.mipmap.", "R.raw.",
-
-        // Android resources
-        "@id/", "@+id/", "@string/", "@color/", "@drawable/", "@mipmap/", "@dimen/",
-        "@style/", "@layout/", "@android:",
-
-        // Common Java/Android libraries
+        // Common Java Standard Libraries
         "ArrayList", "HashMap", "HashSet", "List", "Map", "Set", "String", "Integer",
-        "Boolean", "Double", "Float", "Long", "Object", "Log", "Context", "SharedPreferences",
+        "Boolean", "Double", "Float", "Long", "Object", "Log", "StringBuilder",
+        "StringBuffer", "Math", "System", "Arrays", "Collections",
 
-        // Retrofit/Volley
+        // Retrofit / Volley
         "Retrofit", "OkHttpClient", "Gson", "Call", "Callback", "Response", "RequestQueue",
         "JsonObjectRequest", "StringRequest",
 
-        // Room Database
-        "Room", "Database", "Dao", "Entity", "LiveData", "ViewModel",
-
-        // Firebase
-        "FirebaseDatabase", "FirebaseAuth", "FirebaseStorage", "DatabaseReference",
-        "Query", "ValueEventListener", "ChildEventListener"
+        // R.java References & Resources
+        "R.id.", "R.layout.", "R.string.", "R.drawable.", "R.color.", "R.dimen.",
+        "R.style.", "R.attr.", "R.mipmap.", "R.raw.",
+        "@id/", "@+id/", "@string/", "@color/", "@drawable/", "@mipmap/", "@dimen/",
+        "@style/", "@layout/", "@android:", "@attr/"
     )
 }
--- app/src/main/java/extensions/fora2323/daygreen/keyword/IDXmlKeyword.kt
+++ app/src/main/java/extensions/fora2323/daygreen/keyword/IDXmlKeyword.kt
@@ -1,43 +1,79 @@
 package extensions.fora2323.daygreen.keyword
 
+import kotlin.jvm.JvmField
+
 object IDXmlKeyword {
     @JvmField
     val KEYWORDS = arrayOf(
-        // Layouts
+        // XML Namespaces & Directives
+        "xmlns:android=\"http://schemas.android.com/apk/res/android\"",
+        "xmlns:app=\"http://schemas.android.com/apk/res-auto\"",
+        "xmlns:tools=\"http://schemas.android.com/tools\"",
+        "xml", "encoding", "utf-8", "version",
+
+        // Manifest & Structure Tags
+        "manifest", "application", "activity", "service", "receiver", "provider",
+        "uses-permission", "uses-feature", "intent-filter", "action", "category", "data",
+        "meta-data", "include", "merge", "view", "fragment", "androidx.fragment.app.FragmentContainerView",
+
+        // Drawable & Vector Tags
+        "vector", "path", "group", "clip-path", "selector", "shape", "solid", "stroke",
+        "gradient", "corners", "padding", "size", "ripple", "layer-list", "item", "animated-vector",
+
+        // Standard Layouts & Containers
         "LinearLayout", "RelativeLayout", "FrameLayout", "ConstraintLayout",
-        "CoordinateLayout", "DrawerLayout", "AppBarLayout", "CollapsingToolbarLayout",
+        "CoordinatorLayout", "DrawerLayout", "AppBarLayout", "CollapsingToolbarLayout",
         "CardView", "RecyclerView", "ListView", "GridView", "ScrollView",
         "HorizontalScrollView", "ViewPager", "ViewPager2", "TabLayout", "BottomNavigationView",
 
-        // Views
+        // Standard Views & Material 3 Components
         "TextView", "Button", "ImageView", "EditText", "CheckBox", "RadioButton",
         "RadioGroup", "Spinner", "ProgressBar", "SeekBar", "Switch", "ToggleButton",
         "ImageButton", "FloatingActionButton", "Chip", "MaterialButton", "TextInputLayout",
         "TextInputEditText", "AppCompatTextView", "AppCompatButton", "AppCompatImageView",
+        "ExtendedFloatingActionButton", "MaterialCardView", "MaterialDivider",
+        "LinearProgressIndicator", "CircularProgressIndicator", "Slider", "RangeSlider",
+        "MaterialCheckBox", "MaterialRadioButton", "SwitchMaterial", "SearchBar", "SearchView",
+        "NavigationRailView", "ChipGroup", "ShapeableImageView",
 
-        // Common attributes
-        "android:id", "android:layout_width", "android:layout_height", "android:layout_margin",
-        "android:layout_marginLeft", "android:layout_marginRight", "android:layout_marginTop",
-        "android:layout_marginBottom", "android:layout_marginStart", "android:layout_marginEnd",
-        "android:layout_padding", "android:layout_paddingLeft", "android:layout_paddingRight",
-        "android:layout_paddingTop", "android:layout_paddingBottom", "android:text", "android:textColor",
-        "android:textSize", "android:textStyle", "android:background", "android:gravity",
-        "android:orientation", "android:visibility", "android:src", "android:enabled",
-        "android:clickable", "android:focusable", "android:inputType", "android:hint",
-        "android:maxLines", "android:minLines", "android:maxLength", "android:imeOptions",
-        "android:windowSoftInputMode", "android:adjustViewBounds", "android:scaleType",
-        "android:contentDescription", "android:elevation", "android:alpha", "android:rotation",
+        // Common Android Attributes
+        "android:id", "android:name", "android:layout_width", "android:layout_height",
+        "android:layout_margin", "android:layout_marginLeft", "android:layout_marginRight",
+        "android:layout_marginTop", "android:layout_marginBottom", "android:layout_marginStart",
+        "android:layout_marginEnd", "android:layout_padding", "android:layout_paddingLeft",
+        "android:layout_paddingRight", "android:layout_paddingTop", "android:layout_paddingBottom",
+        "android:layout_gravity", "android:layout_weight", "android:text", "android:textColor",
+        "android:textSize", "android:textStyle", "android:background", "android:foreground",
+        "android:gravity", "android:orientation", "android:visibility", "android:src",
+        "android:enabled", "android:clickable", "android:focusable", "android:inputType",
+        "android:hint", "android:maxLines", "android:minLines", "android:maxLength",
+        "android:imeOptions", "android:windowSoftInputMode", "android:adjustViewBounds",
+        "android:scaleType", "android:contentDescription", "android:elevation", "android:alpha",
+        "android:rotation", "android:clipToPadding", "android:clipChildren", "android:fitsSystemWindows",
+        "android:importantForAccessibility", "android:onClick", "android:tag", "android:theme",
+        "android:style", "android:exported", "android:permission",
 
-        // Values
-        "match_parent", "wrap_content", "@id/", "@+id/", "@string/", "@color/",
-        "@drawable/", "@mipmap/", "@dimen/", "@style/", "@android:color/black",
-        "@android:color/white", "@android:color/transparent", "true", "false",
+        // App & ConstraintLayout Attributes
+        "app:layout_constraintStart_toStartOf", "app:layout_constraintEnd_toEndOf",
+        "app:layout_constraintTop_toTopOf", "app:layout_constraintBottom_toBottomOf",
+        "app:layout_constraintLeft_toLeftOf", "app:layout_constraintRight_toRightOf",
+        "app:layout_constraintBaseline_toBaselineOf", "app:layout_constraintDimensionRatio", "app:layout_behavior",
+        "app:srcCompat", "app:tint", "app:backgroundTint", "app:strokeColor", "app:strokeWidth",
+        "app:cornerRadius", "app:cardCornerRadius", "app:cardElevation", "app:cardBackgroundColor",
+        "app:rippleColor", "app:startIconDrawable", "app:endIconMode", "app:hintEnabled",
+        "app:helperText", "app:errorEnabled", "app:menu", "app:itemIconTint", "app:itemTextColor",
+        "app:title", "app:subtitle", "app:navigationIcon",
 
-        // Tools
-        "tools:context", "tools:text", "tools:ignore", "tools:targetApi",
+        // Tools Attributes
+        "tools:context", "tools:text", "tools:ignore", "tools:targetApi", "tools:listitem",
+        "tools:showIn", "tools:visibility",
 
-        // AppCompat
-        "app:srcCompat", "app:tint", "app:backgroundTint", "app:strokeColor",
-        "app:strokeWidth", "app:cornerRadius", "app:cardElevation"
+        // Values & References
+        "match_parent", "wrap_content", "fill_parent", "@null", "@id/", "@+id/", "@string/",
+        "@color/", "@drawable/", "@mipmap/", "@dimen/", "@style/", "@array/", "@bool/",
+        "@integer/", "@attr/", "@android:color/black", "@android:color/white",
+        "@android:color/transparent", "true", "false", "center", "center_horizontal",
+        "center_vertical", "top", "bottom", "left", "right", "start", "end", "horizontal",
+        "vertical", "gone", "visible", "invisible", "bold", "italic", "normal"
     )
 }
