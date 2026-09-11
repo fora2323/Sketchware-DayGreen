c078fdfd5868d98a8daba8124714b055e9ac1708
fora2323 <fora060823@gmail.com>
2026-09-11 14:19:14 +0800

    fix aapt2 ganarate resource

--- app/src/main/java/org/sketchware/daygreen/Config.kt
+++ app/src/main/java/org/sketchware/daygreen/Config.kt
@@ -1,7 +1,7 @@
 package org.sketchware.daygreen
 
 object Config {
-    const val VAR_DEFAULT_TARGET_COMPILE_VERSION = 37
+    const val VAR_DEFAULT_TARGET_COMPILE_VERSION = 34
     const val VAR_DEFAULT_MIN_SDK_VERSION = 24
     const val VAR_DEFAULT_TARGET_SDK_VERSION = 34
 }
